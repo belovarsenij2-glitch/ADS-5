4f81905fbe2977b20b88812148795148e8ede27c
belovarsenij2-glitch <belovarsenij2@gmail.com>
2026-05-29 17:57:14 +0300

    b102

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,117 +1,97 @@
 // Copyright 2025 NNTU-CS
 #include <string>
 #include <map>
+#include <cctype>
 #include "tstack.h"
 
-int priority(char op) {
-  if (op == '+' || op == '-') return 1;
-  if (op == '*' || op == '/') return 2;
-  return 0;
-}
-
-bool is_operator(char c) {
-  return c == '+' || c == '-' || c == '*' || c == '/';
+int getPriority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
 
 std::string infx2pstfx(const std::string& inf) {
-  TStack<char, 100> stack;
-  std::string post;
+    std::string post = "";
+    TStack<char, 100> stack;
 
-  for (size_t i = 0; i < inf.size(); ++i) {
-    char c = inf[i];
+    for (size_t i = 0; i < inf.length(); ++i) {
+        char c = inf[i];
 
-    if (c == ' ') {
-      continue;
+        if (c == ' ') continue;
+
+        if (std::isdigit(c)) {
+            while (i < inf.length() && std::isdigit(inf[i])) {
+                post += inf[i];
+                i++;
+            }
+            post += ' ';
+            i--;
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (c == ')') {
+            while (!stack.isEmpty() && stack.top() != '(') {
+                post += stack.top();
+                post += ' ';
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!stack.isEmpty() && getPriority(stack.top()) >= getPriority(c)) {
+                post += stack.top();
+                post += ' ';
+                stack.pop();
+            }
+            stack.push(c);
+        }
     }
 
-    if (std::isdigit(c)) {
-      while (i < inf.size() && std::isdigit(inf[i])) {
-        post += inf[i];
-        ++i;
-      }
-      post += ' ';
-      --i;
-    } else if (c == '(') {
-      stack.push(c);
-    } else if (c == ')') {
-      while (!stack.isEmpty() && stack.top() != '(') {
+    while (!stack.isEmpty()) {
         post += stack.top();
         post += ' ';
         stack.pop();
-      }
-      stack.pop();
-    } else if (is_operator(c)) {
-      while (!stack.isEmpty() &&
-             is_operator(stack.top()) &&
-             priority(stack.top()) >= priority(c)) {
-        post += stack.top();
-        post += ' ';
-        stack.pop();
-      }
-      stack.push(c);
     }
-  }
 
-  while (!stack.isEmpty()) {
-    post += stack.top();
-    post += ' ';
-    stack.pop();
-  }
+    if (!post.empty() && post.back() == ' ') {
+        post.pop_back();
+    }
 
-  if (!post.empty() && post.back() == ' ') {
-    post.pop_back();
-  }
-
-  return post;
+    return post;
 }
 
-int eval(const std::string& post) {
-  TStack<int, 100> stack;
+int eval(const std::string& pref) {
+    TStack<int, 100> stack;
 
-  for (size_t i = 0; i < post.size(); ++i) {
-    char c = post[i];
+    for (size_t i = 0; i < pref.length(); ++i) {
+        char c = pref[i];
 
-    if (c == ' ') {
-      continue;
+        if (c == ' ') continue;
+
+        if (std::isdigit(c)) {
+            int num = 0;
+            while (i < pref.length() && std::isdigit(pref[i])) {
+                num = num * 10 + (pref[i] - '0');
+                i++;
+            }
+            stack.push(num);
+            i--;
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            int op2 = stack.top(); stack.pop();
+            int op1 = stack.top(); stack.pop();
+
+            int res = 0;
+            switch (c) {
+                case '+': res = op1 + op2; break;
+                case '-': res = op1 - op2; break;
+                case '*': res = op1 * op2; break;
+                case '/': 
+                    if (op2 != 0) res = op1 / op2;
+                    break;
+            }
+            stack.push(res);
+        }
     }
 
-    if (std::isdigit(c)) {
-      int num = 0;
-
-      while (i < post.size() && std::isdigit(post[i])) {
-        num = num * 10 + (post[i] - '0');
-        ++i;
-      }
-
-      stack.push(num);
-      --i;
-    } else if (is_operator(c)) {
-      int b = stack.top();
-      stack.pop();
-
-      int a = stack.top();
-      stack.pop();
-
-      int result = 0;
-
-      switch (c) {
-        case '+':
-          result = a + b;
-          break;
-        case '-':
-          result = a - b;
-          break;
-        case '*':
-          result = a * b;
-          break;
-        case '/':
-          result = a / b;
-          break;
-      }
-
-      stack.push(result);
-    }
-  }
-
-  return stack.top();
+    return stack.isEmpty() ? 0 : stack.top();
 }
